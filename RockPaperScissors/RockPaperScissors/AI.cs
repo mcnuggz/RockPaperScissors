8878252001d5c760ee94b01666ea3e422d4137bc
Ryan Webb <dzanfox@gmail.com>
2016-01-20 15:55:45 -0600

    added ai, rock, paper, scissors classes

---
+++ RockPaperScissors/RockPaperScissors/AI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class AI
+    {
+        private string aiRock = "Opponent chooses Rock!";
+        private string aiPaper = "Opponent chooses Paper!";
+        private string aiScissors = "Opponent chooses Scissors!";
+    }
+}
